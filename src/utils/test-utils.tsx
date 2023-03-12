21c35580c04012e731e6070828496dd34ccac070
Jun <wong.junwei@petronas.com>
2023-03-12 17:35:01 +0800

    test(UI): added vitest libs and setup

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable import/export */
+import { cleanup, render } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+afterEach(() => {
+  cleanup();
+});
+
+const customRender = (ui: React.ReactElement, options = {}) =>
+  render(ui, {
+    // wrap provider(s) here if needed
+    wrapper: ({ children }) => children,
+    ...options,
+  });
+
+let mock = () => {};
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: any) => {
+    return {
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: mock, // deprecated
+      removeListener: mock, // deprecated
+      addEventListener: mock,
+      removeEventListener: mock,
+      dispatchEvent: mock,
+    };
+  },
+});
+
+export * from "@testing-library/react";
+export { default as userEvent } from "@testing-library/user-event";
+// override render export
+export { customRender as render };
